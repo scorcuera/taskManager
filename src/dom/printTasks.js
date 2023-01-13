715da2a12daa8401f83f6ae7fe191f15a52777c4
scorcuera <scorcuera6@gmail.com>
2023-01-13 11:53:54 +0100

    added styles to task element

--- src/dom/printTasks.js
+++ src/dom/printTasks.js
@@ -2,8 +2,14 @@ export function printTasks(arrayTasks) {
     let templateHTML = "";
     arrayTasks.map(task => {
         templateHTML+= `
-        <div>
-            <h1>${task.name}</h1>
+        <div class="main__container-task">
+            <div class="main__container-task--name">
+                <h1>${task.name}</h1>
+            </div>
+            <div class="main__container-task--icons">
+                <i class="fa-regular fa-pen-to-square"></i>
+                <i class="fa-solid fa-trash"></i>
+            </div>
         </div>
         `
     })
--- src/events/submitTask.js
+++ src/events/submitTask.js
@@ -3,7 +3,7 @@ import { taskHandler } from "../handlers/taskHandler.js";
 
 let submitButtonEl = document.querySelector(".main__form-buttons--submit");
 
-submitButtonEl.addEventListener("click", () => {
+submitButtonEl.addEventListener("click", async () => {
     let inputValue = domRequest.getInputValue();
     console.log(inputValue);
     taskHandler.addTask(inputValue);
